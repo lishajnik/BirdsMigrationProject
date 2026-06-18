b13924fde24fb0fda8f99f10602623c99f5b1fc6
Lishajnik <batrasag@gmail.com>
2026-06-18 11:08:22 +0500

    Подготовка к заливу на pythonanywhere

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder='../../frontend/dist',
+    static_url_path='/',
+    template_folder='../../frontend/dist'
+)
 CORS(app)
 
 import backend.views
